711b110639eb42ea7cd8d2ce2f6109cc8e58d709
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-17 16:31:57 -0300

    screenshots

--- src/main/java/br/luiz/appium/core/BaseTest.java
+++ src/main/java/br/luiz/appium/core/BaseTest.java
@@ -2,15 +2,36 @@ package br.luiz.appium.core;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.commons.io.FileUtils.copyFile;
 
 public class BaseTest {
+    @Rule
+    public TestName testName = new TestName();
     @After
     public void tearDown(){
+        generateScreenshot();
         DriverFactory.getDriver().resetApp();
     }
 
     @AfterClass
     public static void closeApp(){
         DriverFactory.killDriver();
+    }
+    public void generateScreenshot() {
+        File imagen = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+
+        try {
+            copyFile(imagen, new File("target\\screenshots\\"+testName.getMethodName()+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
